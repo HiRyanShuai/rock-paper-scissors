fdd116b9d16c5f6f63fb52adb2aaf24dd6bb8eec
Ryan Shuai <141750879+HiRyanShuai@users.noreply.github.com>
2025-03-25 22:11:04 +0930

    add function getHumanChoice and playGame

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
-console.log("Hello world!");
-
-function getComputerChoice () {
+function getComputerChoice(){
     let choiceIndex = Math.floor( Math.random() * 100 ) % 3;
     let choice = ["rock", "paper", "scissors"];
     return choice[choiceIndex];
 
 }
 
-console.log( getComputerChoice() );
+function getHumanChoice(){
+    return prompt("Input your choice: rock, paper or scissors?");
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+        if(humanChoice===computerChoice){
+            console.log("Draw! " + humanChoice + " vs " + computerChoice);
+        }else if(
+            humanChoice==="rock" && computerChoice==="scissors" || 
+            humanChoice==="scissors" && computerChoice==="paper" ||
+            humanChoice==="paper" && computerChoice==="rock"
+        ){
+            console.log("You win! " + humanChoice + "(you) beats " + computerChoice + "(computer)");
+            humanScore++;
+        }else{
+            console.log("You lose! " + computerChoice + "(computer) beats " + humanChoice + "(you)");
+            computerScore++;
+        }
+        console.log("Score: You = " + humanScore + "  Computer = " + computerScore);
+        return;
+    }    
+    for(let i = 1; i < 6; i++){
+        console.log("Round "+i);
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+    console.log("==== Final result ====")
+    console.log("You = " + humanScore + "  Computer = " + computerScore);
+    if(humanScore > computerScore){
+        console.log("You win!");
+    }else if(humanScore === computerScore){
+        console.log("Draw!");
+    }else{
+        console.log("You lose!");
+    }
+    return;
+}
+
+playGame();
