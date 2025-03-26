aedbf009af3310aab529a5ec20deaa94b9e19785
Ryan Shuai <141750879+HiRyanShuai@users.noreply.github.com>
2025-03-26 20:54:36 +0930

    Change to ` syntax

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ function getComputerChoice(){
     let choiceIndex = Math.floor( Math.random() * 100 ) % 3;
     let choice = ["rock", "paper", "scissors"];
     return choice[choiceIndex];
-
 }
 
 function getHumanChoice(){
@@ -12,30 +11,33 @@ function getHumanChoice(){
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
+
     function playRound(humanChoice, computerChoice){
         humanChoice = humanChoice.toLowerCase();
         if(humanChoice===computerChoice){
-            console.log("Draw! " + humanChoice + " vs " + computerChoice);
+            console.log(`Draw! ${humanChoice} vs ${computerChoice}`);
         }else if(
             humanChoice==="rock" && computerChoice==="scissors" || 
             humanChoice==="scissors" && computerChoice==="paper" ||
             humanChoice==="paper" && computerChoice==="rock"
         ){
-            console.log("You win! " + humanChoice + "(you) beats " + computerChoice + "(computer)");
+            console.log(`You win! ${humanChoice}(you) beats ${computerChoice}(computer)`);
             humanScore++;
         }else{
-            console.log("You lose! " + computerChoice + "(computer) beats " + humanChoice + "(you)");
+            console.log(`You lose! ${computerChoice}(computer) beats ${humanChoice}(you)`);
             computerScore++;
         }
-        console.log("Score: You = " + humanScore + "  Computer = " + computerScore);
+        console.log(`Score: You = ${humanScore}  Computer = ${computerScore}`);
         return;
-    }    
+    }
+
     for(let i = 1; i < 6; i++){
         console.log("Round "+i);
         playRound(getHumanChoice(),getComputerChoice());
     }
+    
     console.log("==== Final result ====")
-    console.log("You = " + humanScore + "  Computer = " + computerScore);
+    console.log(`You = ${humanScore}  Computer = ${computerScore}`);
     if(humanScore > computerScore){
         console.log("You win!");
     }else if(humanScore === computerScore){
